52ac378ed7548d2a480bc7bc9c159012885dc557
marpon <marpon@users.noreply.github.com>
2019-03-21 14:00:34 +0100

    Version 1.3

---
+++ version_1.3/launch_print2pdf.bat
@@ -0,0 +1,7 @@
+
+@echo off
+print2pdf d:\freebasic\testlib\ms_word.doc d:\freebasic\testlib\ms_word_doc0.pdf -d -f
+print2pdf d:\freebasic\testlib\blabla.txt d:\freebasic\testlib\blabla_txt0.pdf -d -a
+print2pdf d:\freebasic\testlib\ms_word.doc d:\freebasic\testlib\ms_word_doc0.pdf -v -f
+print2pdf d:\freebasic\testlib\blabla.txt d:\freebasic\testlib\blabla_txt0.pdf -v -a
+pause
